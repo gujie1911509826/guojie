3c6bc76474bb266e35d78f50c126e5aa4e25825b
guojie <guo.jie2@iwhalecloud.com>
2023-05-28 18:51:49 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface Props{
+  name:string,
+  age:Number,
+}
+const App = (props:Props)=> {
+ const {name , age} = props;
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+{name}:{age}
+ </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App name={'guojie'} age={22} />
   </React.StrictMode>
 );
 
